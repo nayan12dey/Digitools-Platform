a0a68247cc55fe7cf934903046d4b9465f88fd8f
Nayan Dey <ndeynayan123@gmail.com>
2026-03-31 03:04:37 +0530

    add gradient color in active tab and toggle gradient color in between products and cart tab

--- src/App.jsx
+++ src/App.jsx
@@ -47,10 +47,12 @@ function App() {
 
       {/* name of each tab group should be unique */}
       <div className="tabs tabs-box flex justify-center bg-transparent gap-6">
-        <input type="radio" name="my_tabs_1" className="tab rounded-full px-6" aria-label="Products" defaultChecked onClick={() => setActiveTab("products")}/>
-        <input type="radio" name="my_tabs_1" className="tab rounded-full px-6" aria-label={`Cart (${carts.length})`} onClick={() => setActiveTab("cart") }  />
+        <input type="radio" name="my_tabs_1" className={`tab rounded-full px-6 ${activeTab === "products" && ("bg-linear-to-r from-[#4F39F6] to-purple-600 text-white" )}`} aria-label="Products" defaultChecked onClick={() => setActiveTab("products")}/>
+        <input type="radio" name="my_tabs_1" className={`tab rounded-full px-6 ${activeTab === "cart" && ("bg-linear-to-r from-[#4F39F6] to-purple-600 text-white")}`} aria-label={`Cart (${carts.length})`} onClick={() => setActiveTab("cart") }  />
         
       </div>
+
+      
 
        {activeTab === "products" && <ToolsCard productPromise={productPromise} carts={carts} setCarts={setCarts}></ToolsCard>}
 
